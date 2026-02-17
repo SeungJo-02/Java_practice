6ba40b256dbb9985c6d619f9260dce34e2e4de26
seungjo <seungjo.eren@gmail.com>
2026-02-18 02:57:00 +0900

    0218add1

---
+++ src/LevelZero/Solution22.java
@@ -0,0 +1,15 @@
+package LevelZero;
+
+public class Solution22 {
+    public int solution(int a, int b, int c){
+        int result = 0;
+        if (a == b && b == c) {
+            result = (a + b + c) * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c);
+        } else if (a != b && b != c && a != c) {
+            result =  a + b + c;
+
+        } else if (a == b || b == c || a == c) {
+            result =  (a + b + c) * (a * a + b * b + c * c);
+        }return result;
+    }
+}
