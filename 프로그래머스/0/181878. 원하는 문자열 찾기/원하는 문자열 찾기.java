2ca83cf59dbba77e76493643eddd18346f2421ce
SeungJo <binsama0106@gmail.com>
2026-03-01 01:38:48 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 6.92 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,11 @@
+
+
+public class Solution {
+    public int solution(String myString, String pat) {
+        
+       String upper = myString.toUpperCase();
+       String upperfat = pat.toUpperCase();
+        
+        return upper.contains(upperfat) ? 1:0;
+    }
+}
